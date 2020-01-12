6ab9362f046b3ac7c1967297da158609bf0958e2
Ariful Alam <48731800+alamariful1727@users.noreply.github.com>
2020-01-12 14:23:33 +0600

    right sidebar added. (#1)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { RightSidebar } from "../components/RightSidebar";
+import { Icon } from "@blueprintjs/core";
 
 export const Home = () => {
+	const [showDialog, setShowDialog] = useState(false);
 	return (
-		<div>
-			<h1>Home page</h1>
-		</div>
+		<>
+			<div>
+				<h1>Home page</h1>
+				<div>
+					Open your form!!
+					<Icon
+						icon='add'
+						iconSize={20}
+						className='grow pointer tigrow-lightBlue ml2'
+						onClick={() => setShowDialog(!showDialog)}
+					/>
+				</div>
+			</div>
+			{showDialog && (
+				<RightSidebar
+					onClose={() => {
+						setShowDialog(!showDialog);
+					}}
+				>
+					<div className='ph2'>
+						<h1>Put your form component here!!</h1>
+						<small>Click outside for collapse this section.</small>
+					</div>
+				</RightSidebar>
+			)}
+		</>
 	);
 };
